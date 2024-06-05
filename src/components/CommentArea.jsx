8803b6da1ff2ef6c4b1ef1cf8b2fd74b78909063
Gianluca Chiaravalloti <91532537+nagcas@users.noreply.github.com>
2024-06-05 17:35:21 +0200

    Inserito stile delle card

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
+
+import { Spinner } from 'react-bootstrap';
 
 const url = 'https://striveschool-api.herokuapp.com/api/books/';
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjYwMTVmNTA3ZjBkMTAwMTUzZDhjYzUiLCJpYXQiOjE3MTc1NzMxMDksImV4cCI6MTcxODc4MjcwOX0.3cI16-CxRBTWmHyqNW77xPog-2OZ3eDpJI8L6W8hSWQ';
@@ -8,19 +11,25 @@ const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjYwMTVmN
 function CommentArea({ asin }) {
   
   const [comments, setComments] = useState([]);
+  const [isEnableSpinner, setIsEnableSpinner] = useState(false);
 
   useEffect(() => {
+    setIsEnableSpinner(true);
     fetch(url + asin + '/comments/', {
       headers: {
         Authorization: token
       },
       })
       .then((response) => response.json())
-      .then((data) => setComments(data))
+      .then((data) => {
+        setComments(data);
+        setIsEnableSpinner(false);
+      })
   }, [asin]);
 
   return (
     <>
+      {isEnableSpinner && <Spinner animation='grow' />}
       <CommentList comments={comments} />
       <AddComment token={token} elementId={asin} />
     </>
--- src/components/Message.jsx
+++
@@ -1,8 +0,0 @@
-function Message() {
-    return (
-        <>
-        </>
-    );
-}
-
-export default Message;
